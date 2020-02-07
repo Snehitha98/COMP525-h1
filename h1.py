86881eeaa271d18244e3d07539ad68414e726ccb
mihaelasabin <mihaela.sabin@unh.edu>
2020-02-07 08:17:52 -0500

    created starter code

---
+++ h1.py
@@ -0,0 +1,56 @@
+"""
+h1.py
+(replace this with your name)
+(replace this with date completion date)
+"""
+
+
+def my_extend(items, more_items):
+    """
+    Creates and returns a new list that has elements in items to which the
+    the elements in more_items are added.
+    items, more_items: lists
+    Returns: list
+
+    Example:
+    my_exten([1, 2], ['hi']) returns [1, 2, 'hi']
+    """
+
+
+def my_reverse(num_list):
+    """
+    Creates and returns a new list of numbers that has the numbers in num_list
+    but in reverse order
+    num_list: list of numbers
+    Returns: list of numbers
+
+    Example:
+    my_reverse([1, 2, 3]) returns [3, 2, 1]
+
+    Implementation requirements
+      Do NOT use list method reverse
+      Must use the accumulation pattern
+    """
+
+
+def my_translate(word):
+    """
+    Converts the word using the translation rules below and returns it.
+    Translation Rules:
+        last character is moved at the beginning of the word and
+        'ly'is added to the end.
+        If the word is empty, no conversion is applied
+        If the word has one character, the translation is that character
+        followed by 'ly'
+    word: string
+    Returns: string
+
+    Example:
+    my_translate('hi') returns 'ihly',
+    my_translate('bye') returns 'ebyly'
+    """
+
+
+if __name__ == '__main__':
+    # Replace statement pass below with your test cases
+    pass
